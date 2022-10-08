6c52a87cf16a3936904c7eaa8c5fc9ab1e89a323
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-10-08 19:40:04 +0800

    11

---
+++ weather/src/api.js
@@ -0,0 +1,10 @@
+export const geaApiOptions = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': '4c63754473mshd0640a2821f24e1p1630c9jsn53667cbce28e',
+		'X-RapidAPI-Host': 'wft-geo-db.p.rapidapi.com'
+	}
+};
+
+export const GEO_API_URL ="https://wft-geo-db.p.rapidapi.com/v1/geo";
+
