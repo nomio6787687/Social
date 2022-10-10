fd7fb757b6eca1b64e565aec089bec376b633dab
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-10-10 19:04:44 +0800

    s

--- weather/src/components/search/search.js
+++ weather/src/components/search/search.js
@@ -1,43 +1,43 @@
 import { useState } from "react";
 import { AsyncPaginate } from "react-select-async-paginate";
-import {GEO_API_URL, geaApiOptions} from '../../api';
+import { GEO_API_URL, geaApiOptions } from "../../api";
 
-const Search = () => {
+const Search = ({ onSearchChange }) => {
 
     const [search, setSearch] = useState(null);
 
     const loadOptions = (inputValue) => {
         return fetch(
-            '${GEO_API_URL}/adminDivisions?minPopulation=1000000&namePrefix=${inputValue}', 
+            `${GEO_API_URL}/adminDivisions?minPopulation=1000000&namePrefix=${inputValue}`,
             geaApiOptions
-            )
-        .then(response => response.json())
-        .then(response => {
-            return{
-                options: response.data.map((city) =>{
-                    return{
-                        value: `${city.latitude} ${city.longitude}`,
-                        label: `${city.name}, ${city.countryCode}`,
-                    }
-                })
-            }
-        })
-        .catch(err => console.error(err));
-    }
+        )
+            .then(response => response.json())
+            .then(response => {
+                return {
+                    options: response.data.map((city) => {
+                        return {
+                            value: `${city.latitude} ${city.longitude}`,
+                            label: `${city.name}, ${city.countryCode}`,
+                        }
+                    })
+                }
+            })
+            .catch(err => console.error(err));
+    };
 
-    const handleOnChange = (searchData) =>{
+    const handleOnChange = (searchData) => {
         setSearch(searchData);
         onSearchChange(searchData);
-    }
+    };
 
     return (
-       <AsyncPaginate  
-          placeholder="Search for city"
-          debounceTimeout={600}
-          value={search}
-          onChange={handleOnChange}
-          loadOptions={loadOptions}
-       />
+        <AsyncPaginate
+            placeholder="Search for city"
+            debounceTimeout={600}
+            value={search}
+            onChange={handleOnChange}
+            loadOptions={loadOptions}
+        />
     )
 }
 
