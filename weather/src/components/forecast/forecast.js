670d7cdb158dbe905cc9109e64e714073b0b3fde
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-10-11 12:53:51 +0800

    cv

--- weather/src/components/forecast/forecast.js
+++ weather/src/components/forecast/forecast.js
@@ -1,5 +1,25 @@
-const Forecast = () =>{
-    return 'hello';
+import { 
+    Accordion, 
+    AccordionItemHeading,
+    AccordionItem,
+    AccordionItemPanel,
+} from "react-accessible-accordion";
+
+const Forecast = ({data}) =>{
+    return 
+    <>
+       <label className="title">Daitly</label> 
+       <Accordion allowZeroExpanded>
+        {data.list.splice(0, 7).map((item, idx) => (
+            <AccordionItem key={idx}>
+                <AccordionItemHeading>
+                    
+                </AccordionItemHeading>
+            </AccordionItem>
+        ))}
+       </Accordion>
+
+    </>;
 }
 
 export default Forecast;
