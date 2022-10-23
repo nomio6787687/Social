c6cddc403986df148eefb51da7b08b28cd10d1aa
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-10-23 17:25:16 +0800

    cfgc

--- nomin3/src/components/Navbar.jsx
+++ nomin3/src/components/Navbar.jsx
@@ -1,8 +1,14 @@
 import React from 'react'
 
+import {MenuIcon, XIcon} from '@heroicons/react/outline'
+
 const Navbar = () => {
     return (
-        <div>Navbar</div>
+        <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
+
+
+            <MenuIcon className='w-5' />
+        </div>
     )
 }
 
