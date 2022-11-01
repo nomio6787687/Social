cf1b0e222a491208da142795b6b9c6377248e7fe
Tsendee <btseegii329@gmail.com>
2022-11-01 12:27:59 +0800

    qw

--- cloud/src/components/Navbar.jsx
+++ cloud/src/components/Navbar.jsx
@@ -1,15 +1,13 @@
-import React from 'react'
+import React from "react";
 
-import {MenuIcon, XIcon} from '@heroicons/react/outline'
+import { MenuIcon, XIcon } from "@heroicons/react/outline";
 
 const Navbar = () => {
-    return (
-        <div className='w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg'>
+  return (
+    <div className="w-screen h-[80px] z-10 bg-zinc-200 fixed drop-shadlow-lg">
+      <MenuIcon className="w-5" />
+    </div>
+  );
+};
 
-
-            <MenuIcon className='w-5' />
-        </div>
-    )
-}
-
-export default Navbar
+export default Navbar;
