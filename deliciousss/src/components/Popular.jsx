10ddfe20ac3445529f0e6ab16d7de75d305f7335
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-11-12 22:03:52 +0800

    d

--- deliciousss/src/components/Popular.jsx
+++ deliciousss/src/components/Popular.jsx
@@ -1,14 +1,50 @@
+import { useEffect, useState} from "react";
+import style from "styled-components";
+
 function Popular() {
-    const getPopular = async () => {
-        const api = await fetch(`https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`);
-        const data = await api.json();
-        console.log(data);
-    }
-
-
+    const [popular, setPopular] = useState([]);
     
-    return <div>Popular</div>;
+  useEffect(() => {
+    getPopular();
+  },[]);
+
+    const getPopular = async () => {
+        const api = await fetch(
+            `https://api.spoonacular.com/recipes/random?apiKey=${process.env.REACT_APP_API_KEY}&number=9`
+        );
+        const data = await api.json();
+        setPopular(data.recipes);
+        console.log(data.recipes);
+    };
+
+    return (
+        <div>
+            {popular.map((recipe) => {
+                return (
+                    <Wrapper>
+                        <h3>Popular Picks</h3>
+                        {popular.map((recipe) => {
+                           return(
+                            <Card>
+                                <p>{recipe.title}</p>
+                            </Card>
+                           );
+                        })}
+                    </Wrapper>
+                );
+            })}
+        </div>
+    );
 }
 
+const Wrapper = style.div`
+   margin: 4rem 0rem;
+`;
+
+const Card =style.div`
+   min-height: 25rem;
+   border-radius: 2rem;
+`
+
 export default Popular;
-   
+    
