58f2c1a2c107e89f4d9e4be482eade4d1bbeaf3e
Nudkhuu <davaadashnudkhuu@gmail.com>
2022-11-15 13:43:45 +0800

    n

--- socialapp/src/pages/home/Home.jsx
+++ socialapp/src/pages/home/Home.jsx
@@ -2,29 +2,29 @@ import Topbar from "../../components/topbar/Topbar";
 import Sidebar from "../../components/sidebar/Sidebar";
 import Feed from "../../components/feed/Feed";
 import Rightbar from "../../components/rightbar/Rightbar";
-import "./home.css"
+import "./home.css";
 import { useEffect, useState } from "react";
 import { sendRequest, urlLookup } from "../../settings/settings";
 
 export default function Home() {
-    const [datas, setDatas] = useState();
+  const [datas, setDatas] = useState();
 
-    useEffect(() =>{
-        const bodyChiglel = {
-            action: "chiglel"
-        };
-        sendRequest(urlLookup, bodyChiglel).then((data) => setDatas(data));
-    },[])
-   
-    return (
-     <>
-     <p>{datas&&JSON.stringify(datas)}</p>
-        <Topbar/>
-        <div className="homeContainer">
-        <Sidebar/>
-        <Feed/>
-        <Rightbar/>
-        </div>
+  useEffect(() => {
+    const bodyChiglel = {
+      action: "chiglel",
+    };
+    sendRequest(urlLookup, bodyChiglel).then((data) => setDatas(data));
+  }, []);
+
+  return (
+    <>
+      <p>{datas && JSON.stringify(datas)}</p>
+      <Topbar />
+      <div className="homeContainer">
+        <Sidebar />
+        <Feed />
+        <Rightbar />
+      </div>
     </>
-    );
+  );
 }
