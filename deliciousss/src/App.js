9503f66f05814d00746d9a3bc0b22c5febe19985
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-11-19 19:02:50 +0800

    q

--- deliciousss/src/App.js
+++ deliciousss/src/App.js
@@ -1,12 +1,14 @@
 import Pages from "./pages/Pages";
+import Category from "./components/Category";
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello</h1>
+      <Category/>
       <Pages/>
     </div>
   );
 }
 
 export default App ;
+   
