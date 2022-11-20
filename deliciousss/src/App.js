9ea55388d654043bb0e6faf35b7d36217188b5ad
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-11-20 14:06:33 +0800

    gj

--- deliciousss/src/App.js
+++ deliciousss/src/App.js
@@ -1,14 +1,17 @@
 import Pages from "./pages/Pages";
 import Category from "./components/Category";
+import {BrowserRouter} from 'react-router-dom'
 
 function App() {
   return (
     <div className="App">
+      <BrowserRouter>
       <Category/>
       <Pages/>
+      </BrowserRouter>
     </div>
   );
 }
 
 export default App ;
-   
+    
