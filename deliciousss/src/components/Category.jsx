85c5b2293cc265d5fa1b129c32a195ae0b17adec
nomio6787687 <gd21d001@mandakh.edu.mn>
2022-11-20 11:47:41 +0800

    d

--- deliciousss/src/components/Category.jsx
+++ deliciousss/src/components/Category.jsx
@@ -1,6 +1,7 @@
 import { FaPizzaSlice, FaHamburger } from "react-icons/fa";
 import { GiNoodles, GiChopsticks } from "react-icons/gi";
 import styled from "styled-components";
+import {NavLink} from 'react-router-dom';
 
 function Category(){
     return (
