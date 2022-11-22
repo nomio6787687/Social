4ac5b87059ba19259236f239263455121e4cd303
dorjjj <gd21d001@mandakh.edu.mn>
2022-11-22 16:13:10 +0800

    Update index.js

--- socialapp/src/index.js
+++ socialapp/src/index.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+// import ReactDOM from 'react-dom';
 import App from './App';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
@@ -7,4 +8,5 @@ root.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>
+
 );
