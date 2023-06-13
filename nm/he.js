e9cd3431665a5e41dd9e58f29bf5be11791826d6
nomin <gd21d001@mandakh.edu.mn>
2023-06-14 02:36:13 +0800

    d

---
+++ nm/he.js
@@ -0,0 +1,6 @@
+mobiscroll.select('#single-group-select', {
+    inputElement: document.getElementById('my-input'),
+    touchUi: false
+});
+var x = pcFeatures.GetTabByName("consignerCoverTab");  
+x.SetEnabled(true); 
